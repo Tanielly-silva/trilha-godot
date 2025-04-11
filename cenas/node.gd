3560b1b29996b414922d482ea2f91a2eb6469a78
Tânielly Silva <frourzinhass@gmail.com>
2025-04-11 16:28:11 -0400

    Cenas clicks!
    A cada click um block 😵 😄

--- cenas/node.gd
+++ cenas/node.gd
@@ -1,20 +1,22 @@
 extends Node
 
-#func_ready() -> void:
-	#pass
-	#
-#func _process (delta: float) -> void:
-	#pass
-	
+@export var object_templates: Array[PackedScene]
+
 func _input(event: InputEvent) -> void:
 	#input no botao e um evento de click
 	if event is InputEventMouseButton:
 		if event.button_index == 1:
 			if event.pressed:
-				spaw_object	(event.position):
+				spaw_object(event.position)
 	
 	pass
 		
-func spaw_object(event.position) -> void:
+func spaw_object(position: Vector2) -> void:
+	var index: int = randi_range(0, object_templates.size() -1)
+	var object_template = object_templates[index]
+	var object: RigidBody2D = object_template.instantiate()
+	object.position = position
+	add_child(object)
+	
 	pass
 			
