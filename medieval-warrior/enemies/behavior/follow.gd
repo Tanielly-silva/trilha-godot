a9051253eec785174931f0e19aabc2d48f3fbf9e
Tânielly Silva <frourzinhass@gmail.com>
2025-05-14 17:54:42 -0300

    Creat moved game manager
    Game manager, a fofoqueira, manda recado para os inimigos onde o roxinho está através do script novo. O peão também agora sabe virar o rosto.

--- medieval-warrior/enemies/behavior/follow.gd
+++ medieval-warrior/enemies/behavior/follow.gd
@@ -2,9 +2,22 @@ extends CharacterBody2D
 
 @export var speed = 1.0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
+	var player_position = GameManager.player_position
+	var diference = player_position - position
+	var input_vector = diference.normalized()
 
-#input vector
-#velocity = input vector * seed * 100.0
 
-move_and_slide()
+#input vector = vector = vector2 entre -1 e 1 em ambos os eixos
+	velocity = input_vector * speed * 100.0
+
+	move_and_slide()
+	
+	
+		# Girar o sprite 	
+	if input_vector.x > 0:
+		sprite.flip_h = false
+	elif input_vector.x < 0:		
+		sprite.flip_h = true
